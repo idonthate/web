81674d7e48489bcc40ba37976f24684fcde55e26
idonthate <157199072+idonthate@users.noreply.github.com>
2024-01-19 22:47:36 -0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+
+st.set_page_config(page_title = "nunya", page_icon = ":🙈:")
+
+
+st.subheader("HI im duck")
+st.title("im a cuck")
+st.write("i like cuuum")
+st.write("[learn more]")
+
